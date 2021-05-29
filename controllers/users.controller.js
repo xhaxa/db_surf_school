3b7d0454306003022a18b4f13d55fda1fd6a63c1
Sara <you@example.comxhaxadesign@gmail.com>
2021-05-29 12:22:26 +0100

    Router & controller SIGNUP& LOGIN - OK

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,1 +1,12 @@
 const usersModel = require('../models/users.model')
+
+function getAllUsers(req, res) {
+  usersModel.find()
+    .then((users) => {
+      res.json(users)
+    })
+
+    .catch((err) => {
+      res.json(err)
+    })
+}
