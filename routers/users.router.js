34ce1b18ad40e01ed9e11c56882534a8bbcd21de
Sara <you@example.comxhaxadesign@gmail.com>
2021-06-02 01:42:31 +0100

    wip => user/me ... / user(admin) OK

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,7 +1,7 @@
 const usersRouter = require('express').Router()
 
 const {admin, auth} = require('../utils/function')
-const { getAllUsers, seeYourUser, seeOneUser, modifyUser, deleteUser, addStudentToUser, seeUsersStudentList } = require('../controllers/users.controller')
+const { getAllUsers, seeYourUser, seeOneUser, modifyUser, deleteUser, addStudentToUser, seeUsersStudentList, deleteStudentFromUser } = require('../controllers/users.controller')
 
 usersRouter.get('/', auth, admin, getAllUsers) 
 usersRouter.get('/me', auth, seeYourUser)
@@ -9,8 +9,8 @@ usersRouter.get('/:userId', auth, admin, seeOneUser)
 usersRouter.put('/:userId', auth, admin, modifyUser)
 usersRouter.delete('/:userId', auth, admin, deleteUser)
 usersRouter.put('/:userId/students/add', auth, admin, addStudentToUser)
-usersRouter.get('/:userId/student', seeUsersStudentList)
-
+usersRouter.get('/:userId/students', auth, admin, seeUsersStudentList)
+usersRouter.delete('/:userId/students/delete', auth, admin,  deleteStudentFromUser)
 
 
 
